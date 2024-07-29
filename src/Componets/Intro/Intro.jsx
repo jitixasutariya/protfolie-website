56f25a6c4e5dfe293ba41b42e286da36e7311806
jitixa <jitixa.kothiya@envertis.com.au>
2024-07-30 09:32:36 +1000

    add comment

--- src/Componets/Intro/Intro.jsx
+++ src/Componets/Intro/Intro.jsx
@@ -5,6 +5,7 @@ import { Link } from "react-scroll";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBriefcase } from "@fortawesome/free-solid-svg-icons";
 
+// IconButton component renders a button with an icon and text
 const IconButton = () => {
   return (
     <Link>
@@ -18,30 +19,36 @@ const IconButton = () => {
             margin: "0 0.65rem",
           }}
         />
-        <spna className="hireMe" style={{ fontWeight: "bolder" }}>
+        {/* The "Hire Me" button text, styled to be bold using inline styles */}
+        <span className="hireMe" style={{ fontWeight: "bolder" }}>
           Hire Me
-        </spna>
+        </span>
       </button>
     </Link>
   );
 };
 
+// Intro component represents the introduction section of the webpage
 const Intro = () => {
   return (
     <section id="intro">
       <div className="introContent">
         <span className="hello">Hello,</span>
         <span className="introText">
+          {/* Display the developer's name */}
           I'm <span className="introName">Jitixa</span>
           <br /> Full-Stack Developer
         </span>
         <p className="introPara">
+          {/* Brief description of skills and expertise */}
           I am a versatile Full-Stack Developer skilled in building
           comprehensive web applications <br /> with expertise in both front-end
           and back-end technologies.
         </p>
+        {/* Render the IconButton component */}
         <IconButton />
 
+        {/* Display the developer's image */}
         <img src={personImg} alt="Jitixa" className="selfImg" />
       </div>
     </section>
--- src/Componets/NavBar/NavBar.jsx
+++ src/Componets/NavBar/NavBar.jsx
@@ -7,15 +7,20 @@ import { Link } from "react-scroll";
 const NavBar = () => {
   return (
     <nav className="navbar">
+      {/* Logo Image */}
       <img src={logo} alt="Logo" className="logo" />
+
+      {/* Desktop Menu Links */}
       <div className="desktopMenu">
         <Link className="desktopListItem">Home</Link>
         <Link className="desktopListItem">AboutUs</Link>
         <Link className="desktopListItem">Protfolio</Link>
         <Link className="desktopListItem">Clients</Link>
       </div>
+
+      {/* Contact Button */}
       <button className="desktopMenuBtn">
-        <img src={contact} alt="" className="desktopImg" />
+        <img src={contact} alt="Contact Icon" className="desktopImg" />
         <div class="text">Contact Me</div>
       </button>
     </nav>
